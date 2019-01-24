289ede6c9fbc07fc703830d104ef2b4adbc9a3fa
Marcus Alsterfjord <marcus@marcussite.se>
2019-01-24 22:05:48 +0100

    Added crontask for watching api-feed for changes

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ const fetch = require('node-fetch');
 
 const trigger_alert = "krisinformation_trigger_alert";
 
-const cities = [
-	'Helsingborg',
-	'Höganäs',
-	'Östergötlands län'
-];
+const cronName = "crisisInformationCronTask"
+const cronInterval = "*/10 * * * * *"; // "0 */5 * * * *";
+
+let krisinformationTrigger;
 
 class Krisinformation extends Homey.App {
 
@@ -20,28 +19,64 @@ class Krisinformation extends Homey.App {
 		// Register FlowCardTrigger
 		krisinformationTrigger = new Homey.FlowCardTrigger(trigger_alert).register();
 
+		// Register crontask
+		Homey.ManagerCron.getTask(cronName)
+			.then(task => {
+				this.log("The task exists: " + cronName);
+				task.on('run', () => this.checkCrisisInformation());
+			})
+			.catch(err => {
+				if (err.code == 404) {
+					this.log("The task has not been registered yet, registering task: " + cronName);
+					Homey.ManagerCron.registerTask(cronName, cronInterval, null)
+						.then(task => {
+							task.on('run', () => this.checkCrisisInformation());
+						})
+						.catch(err => {
+							this.log(`problem with registering cronjob: ${err.message}`);
+						});
+				} else {
+					this.log(`other cron error: ${err.message}`);
+				}
+			});
+
+		// Run on unload
+		Homey
+			.on('unload', () => {
+				Homey.ManagerCron.unregisterTask(cronName);
+			});
+
 	}
 
-	getCrisisInformation() {
+	checkCrisisInformation() {
 
-		let apiFetch = fetch('http://api.krisinformation.se/v1/feed?format=json')
-			.then(response => response.json())
-			.then(json => json.Entries)
-			.then(data => {
-				let warnings = data.filter(item => {
-						return searchForCity(item);
-					})
-					.map(item => {
-						return {
-							Updated: item.Updated,
-							Published: item.Published,
-							Title: item.Title,
-							Text: item.Summary
-						}
-					})
+		let citiesSettingsString = Homey.ManagerSettings.get('cities');
 
-				console.log(warnings)
-			})
+		let cities = (IsJsonString(citiesSettingsString)) ? JSON.parse(citiesSettingsString) : undefined;
+
+		if (cities !== undefined) {
+			fetch('http://api.krisinformation.se/v1/feed?format=json')
+				.then(response => response.json())
+				.then(json => json.Entries)
+				.then(data => {
+					let warnings = data.filter(item => {
+							return searchForCity(item);
+						})
+						.map(item => {
+							return {
+								// Updated: item.Updated,
+								// Published: item.Published,
+								Title: item.Title,
+								Message: item.Summary
+							}
+						})
+
+					console.log("Searching for:", cities)
+					console.log("Found:", warnings)
+					if(warnings.length > 0)
+						krisinformationTrigger.trigger(warnings[0])
+				})
+		}
 
 		function searchForCity(item) {
 			let found = false;
@@ -56,6 +91,15 @@ class Krisinformation extends Homey.App {
 				})
 			})
 			return found;
+		}
+
+		function IsJsonString(str) {
+			try {
+				JSON.parse(str);
+			} catch (e) {
+				return false;
+			}
+			return true;
 		}
 	}
 }
